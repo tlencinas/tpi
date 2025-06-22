b534a1fcfba61932a0c5a83bc858d3f54a35c70e
Tomás Lencinas <tomilencinas97@gmail.com>
2025-06-21 22:01:52 -0300

    Etapa 1 completada.

---
+++ dds-frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import Inicio from "./components/Inicio";
+
+function App() {
+  return (
+     <>
+      <Inicio/>
+    </>
+  );
+}
+export default App;
---
+++ dds-frontend/src/components/Inicio.jsx
@@ -0,0 +1,20 @@
+function Inicio() {
+  return (
+     <div className="mt-4 p-5 rounded" style={{backgroundColor:"lightgray"}} >
+        <h1>Pymes 2025</h1>
+        <p>Este ejemplo está desarrollado con las siguientes tecnologías:</p>
+        <p>
+          Backend: NodeJs, Express , WebApiRest, Swagger, Sequelize, Sqlite y Javascript.
+        </p>
+        <p>
+          Frontend: Single Page Aplication, HTML, CSS, Bootstrap, NodeJs,
+          Javascript y React.
+        </p>
+        <button className="btn btn-lg btn-primary">
+          <i className="fa fa-search p-1"> </i>
+          Ver Categorias
+        </button>
+      </div>
+  );
+}
+export default Inicio;
